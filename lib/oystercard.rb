bf33af34f564a5e4504d75e840e5003bb30b5248
djvcom <djverrall@gmail.com>
2019-07-01 16:11:38 +0100

    Added error handling for top up, and touch in

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,30 @@
 class Oystercard
+  attr_reader :balance, :in_journey
+
+  OYSTER_CAP = 90.0
+  MINIMUM_BALANCE = 1.0
+
+  def initialize
+    @balance = 0
+    @in_journey = false
+  end
+
+  def top_up(amount = 0)
+    raise "Oyster balance cannot exceed #{OYSTER_CAP} GBP" if (@balance + amount) > OYSTER_CAP
+    @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def touch_in(station = "Default")
+    raise "Insufficient Balance" if @balance < MINIMUM_BALANCE
+    @in_journey = true
+  end
+
+  def touch_out(station = "Default")
+    @in_journey = false
+  end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,56 @@
 require 'oystercard'
 
 describe Oystercard do
+  it "should have a balance of 0" do
+    expect(subject.balance).to eq 0
+  end
+end
 
+describe Oystercard do
+  it "should check if the user is currently on a journey" do
+    expect(subject.in_journey).to eq false
+  end
+  it "should set in_journey to true once tapped in" do
+    subject.touch_in
+    expect(subject.in_journey).to eq true
+  end
+  it "should set in_journey to false when tapped out" do
+    subject.touch_in
+    subject.touch_out
+    expect(subject.in_journey).to eq false
+  end
+end
+
+describe Oystercard do
+  describe '#touch_in' do
+    it "should error if user tries to touch_in with a balance lower than £1" do
+      subject.top_up(0.50)
+      expect{ subject.touch_in }.to raise_error "Insufficient Balance"
+    end
+  end
+end
+
+describe Oystercard do
+  describe "top_up" do
+    it "should top up the balance with the amount entered" do
+      oystercard1 = Oystercard.new
+      oystercard1.top_up(3.00)
+      expect(oystercard1.balance).to eq 3.00
+    end
+  end
+  describe "#top_up" do
+    it "should raise an exception when balance will be greater than 90 GBP" do
+      max_balance = Oystercard::OYSTER_CAP
+      expect{ subject.top_up(91.00)}.to raise_error "Oyster balance cannot exceed #{max_balance} GBP"
+    end
+  end
+end
+
+describe Oystercard do
+  describe 'deduct' do
+    it "should deduct an amount from the current balance" do
+      subject.top_up(50.00)
+      expect(subject.deduct(20.00)).to eq 30.0
+    end
+  end
 end
