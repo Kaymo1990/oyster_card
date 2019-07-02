2347103c9322e2d0af0a8e11c583a81fbb0c6bd9
djvcom <djverrall@gmail.com>
2019-07-02 09:48:28 +0100

    Refactored a little for easier reading and renamed variables and global constants for easier readability.

--- Gemfile
+++ Gemfile
@@ -1,10 +1,8 @@
-
-source "https://rubygems.org"
-
+source 'https://rubygems.org'
 ruby '2.6.0'
 
 group :development, :test do
   gem 'rspec'
 end
 
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,30 +1,44 @@
 class Oystercard
-  attr_reader :balance, :in_journey
+  attr_reader :balance, :in_journey, :name, :entry_station, :list_journeys
 
-  OYSTER_CAP = 90.0
+  MAXIMUM_BALANCE = 90.0
   MINIMUM_BALANCE = 1.0
+  MINIMUM_FARE = 1.0
 
   def initialize
     @balance = 0
-    @in_journey = false
+    @entry_station = nil
+    @journeys = []
   end
 
   def top_up(amount = 0)
-    raise "Oyster balance cannot exceed #{OYSTER_CAP} GBP" if (@balance + amount) > OYSTER_CAP
+    raise "Oyster balance cannot exceed #{MAXIMUM_BALANCE} GBP" if (@balance + amount) > MAXIMUM_BALANCE
     @balance += amount
   end
 
-  def deduct(amount)
+  def in_journey
+    !@entry_station.nil?
+  end
+
+  def touch_in(station = 'Default')
+    raise 'Insufficient Balance' if @balance < MINIMUM_BALANCE
+    @entry_station = station
+  end
+
+  def touch_out(station = 'Default')
+    deduct
+    @journeys.push({:entry_station => @entry_station, :exit_station => station})
+    @entry_station = nil
+  end
+
+  def list_journeys
+    @journeys
+  end
+
+  private
+
+  def deduct(amount = MINIMUM_FARE)
     @balance -= amount
-  end
-
-  def touch_in(station = "Default")
-    raise "Insufficient Balance" if @balance < MINIMUM_BALANCE
-    @in_journey = true
-  end
-
-  def touch_out(station = "Default")
-    @in_journey = false
   end
 
 end
